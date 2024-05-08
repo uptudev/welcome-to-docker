002f8bb87243e9a9e16be9f4efd743b160f9e0ad
uptu <uptu@uptu.dev>
2024-05-08 16:15:34 -0600

    Fixed SSR generation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Docker</h1>
+<p>You have successfully run a Docker container!</p>
